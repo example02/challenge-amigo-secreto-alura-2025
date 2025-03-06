3159e0aa4bc606ba98f4b4202bffd6354729ec64
wmm02 <w.example10@gmail.com>
2025-03-06 13:06:56 -0500

    Se crea la función Agregar amigos

--- app.js
+++ app.js
@@ -3,4 +3,20 @@
 // Se declara y crea una lista llamada amigos.
 let listaAmigos = [];
 let indiceSorteado;
+// Se crea la función funcion  Agregar amigos. 
+function  agregarAmigos() {
+    let nombreCaja = document.getElementById('amigo').value;
+    // condicion que no permite ingresar un valor "" y tampoco un número.
+        if (nombreCaja != ""  ) {
+            if(typeof nombreCaja === "string" && isNaN(nombreCaja)) {
+            listaAmigos.push(nombreCaja);
+            //console.log(nombreCaja);
+            //console.log(listaAmigos);
+            } else {
+                alert('Por favor, ingrese un nombre valido');
+            }
+        } else {
+            alert("Por favor, inserte un nombre.");
+        }
+}
 
