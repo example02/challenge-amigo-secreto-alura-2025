2c1d6a65882fdafa207be56af92574b851a0b2e4
wmm02 <w.example10@gmail.com>
2025-03-06 13:04:36 -0500

    Se declara variable indiceSorteado

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
- 
-// Desafío amigo secreto 
-// Se declara y crea una lista llamada amigos 
-let amigos = [];
+// Desafío amigo secreto. 
 
+// Se declara y crea una lista llamada amigos.
+let listaAmigos = [];
+let indiceSorteado;
 
